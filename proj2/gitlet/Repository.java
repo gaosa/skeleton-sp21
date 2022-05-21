ddd3b6c156571aa532f1debecc8e052b94a61945
Siang Gao <siang_gao@apple.com>
2022-05-20 22:25:08 -0700

    finish init

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,11 @@
 package gitlet;
 
 import java.io.File;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -11,7 +16,7 @@ import static gitlet.Utils.*;
  *
  *  @author TODO
  */
-public class Repository {
+public class Repository implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -24,6 +29,10 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
+    // All the branches
+    // Key is branch name, value is sha1 of commit
+    private Map<String, String> branches;
+    private String currentBranch;
 
     /* TODO: fill in the rest of this class. */
     public static void init() throws GitletException {
@@ -33,5 +42,26 @@ public class Repository {
         if (!GITLET_DIR.mkdir()) {
             throw new GitletException("Cannot init Gitlet.");
         }
+
+        // Create and save the initial commit
+        // new Date(0) is the time 0
+        Commit initialCommit = new Commit("initial commit", new Date(0));
+        byte[] content = Utils.serialize(initialCommit);
+        String sha1 = Utils.sha1(content);
+        Utils.writeObject(new File(GITLET_DIR, sha1), content);
+
+        // Save metadata (branches, current branch)
+        new Repository(Collections.singletonMap("master", sha1), "master").save();
+    }
+
+    // This is private because only init() called this function
+    private Repository(Map<String, String> branches, String currentBranch) {
+        this.branches = branches;
+        this.currentBranch = currentBranch;
+    }
+
+    // Save metadata (branches, current branch)
+    public void save() {
+        Utils.writeObject(new File(GITLET_DIR, "metadata"), Utils.serialize(this));
     }
 }
